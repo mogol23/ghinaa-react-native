55d8607a6b478e268862f72c77238b611284e533
Ahmad Habib <achmadfaz@gmail.com>
2020-04-20 17:00:37 +0700

    add form ortu
    save data ortu

--- src/pages/ProfileUpdate/index.js
+++ src/pages/ProfileUpdate/index.js
@@ -3,10 +3,14 @@ import { Text, View } from 'react-native';
 import { AuthContext } from '../../provider/AuthProvider';
 import FormSantri from './FormSantri';
 import FormAlumni from './FormAlumni';
+import FormOrtu from './FormOrtu';
 
 const ProfileUpdate = () => {
   const {user} = useContext(AuthContext);
   
+  if(user.user.role_id == 2){
+    return <FormOrtu />
+  }
   if(user.user.role_id == 3){
     return <FormSantri />
   }
